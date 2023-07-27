b141db9be1746cb3348d3fae22bfbc064f1f69f0
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-27 18:27:13 +0500

    feat: implement user registeration page.
    * RegisterationForm: user can add require information.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login'
 import './index.css'
+import Registeration from './Pages/Registeration'
 function App() {
 	return (
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<Login />} />
+				<Route path="/Registeration" element={<Registeration />}></Route>
 			</Routes>
 		</div>
 	)
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Input(props) {
 	return (
 		<input
-			className="border border-gray-300 outline-none px-4 py-2 mt-2"
+			className="border border-gray-300 outline-none w-80 px-4 py-2 mt-2 font-light"
 			placeholder={props.placeholder}
 			name={props.name}
 			onChange={props.onChangeHandler}
