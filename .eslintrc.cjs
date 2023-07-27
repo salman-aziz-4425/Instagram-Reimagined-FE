3dcc4ffdde144a823d2ccc06d56bc07f5cc7ef89
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-27 14:21:59 +0500

    initial project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+	root: true,
+	env: { browser: true, es2021: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended'
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true }
+		],
+		'react/prop-types': 'off',
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'react/prop-types': 0,
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'error'
+	}
+}
