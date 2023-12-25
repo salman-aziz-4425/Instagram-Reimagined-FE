64af892b98b775304387933decf1f1ab91d2a7e5
Muhammad Salman Aziz <85288719+salman-aziz-4425@users.noreply.github.com>
2023-12-25 22:42:55 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss]
+	plugins: [react(), tailwindcss],
 	server:{
 		host:true,
 		strictPort:true,
